04b50d461068078c810dc86985a788de7456b4e9
kuzojman <vladimir.kuzovkin@bk.ru>
2021-01-29 22:23:06 +0300

    Initial commit

--- main.py
+++ main.py
@@ -2,151 +2,287 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+title = "Stepik Travel"
+subtitle = "Для тех, кого отвлекают дома"
+description = """Лучшие направления, где никто не будет вам мешать сидеть на берегу и изучать программирование, 
+дизайн, разработку игр и управление продуктами """
+departures = {"msk": "Из Москвы", "spb": "Из Петербурга", "nsk": "Из Новосибирска", "ekb": "Из Екатеринбурга",
+              "kazan": "Из Казани"}
 
-points = [
-  "951 Marsh StreetMiddleburg, FL 32068",
-  "207 Westport Drive Uniondale, NY 11553",
-  "1 Second AvenueWayne, NJ 07470",
-]
+tours = {
+    1: {
+        "title": "Marina Lake Hotel & Spa",
+        "description": "Отель выглядит уютно. Он был построен из красного соснового дерева и украшен синими камнями.  "
+                       "Высокие округлые окна добавляют общий стиль дома и были добавлены в дом в довольно "
+                       "симметричном образце.",
+        "departure": "nsk",
+        "picture": "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 62000,
+        "stars": "4",
+        "country": "Куба",
+        "nights": 6,
+        "date": "2 марта",
+    },
+    2: {
+        "title": "Baroque Hotel",
+        "description": "Здание отеля имеет форму короткой буквы U. Два расширения связаны стеклянными нависающими "
+                       "панелями. Второй этаж такого же размера, как и первый, который был построен точно над полом "
+                       "под ним. Этот этаж имеет совершенно другой стиль, чем этаж ниже.",
+        "departure": "ekb",
+        "picture": "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 85000,
+        "stars": "5",
+        "country": "Вьетнам",
+        "nights": 8,
+        "date": "12 января",
+    },
+    3: {
+        "title": "Voyager Resort",
+        "description": "Снаружи отель выглядит красиво и традиционно. Он был построен с белыми камнями и имеет еловые "
+                       "деревянные украшения. Высокие, большие окна добавляют к общему стилю дома и были добавлены в "
+                       "дом в основном симметричным способом.",
+        "departure": "nsk",
+        "picture": "https://images.unsplash.com/photo-1569660072562-48a035e65c30?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 63000,
+        "stars": "3",
+        "country": "Пакистан",
+        "nights": 11,
+        "date": "7 февраля",
+    },
+    4: {
+        "title": "Orbit Hotel",
+        "description": "Каждый домик оборудован средней кухней и одной небольшой ванной комнатой, в нем также есть "
+                       "уютная гостиная, две спальни, скромная столовая и большой подвал.  Небольшие треугольные окна "
+                       "добавляют к общему стилю дома и были добавлены в дом в основном симметричным способом.",
+        "departure": "msk",
+        "picture": "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 62000,
+        "stars": "4",
+        "country": "Индия",
+        "nights": 9,
+        "date": "22 января",
+    },
+    5: {
+        "title": "Atlantis Cabin Hotel",
+        "description": "Этот дом среднего размера имеет футуристический вид и находится в среднем состоянии. Интерьер "
+                       "выполнен в насыщенных тонах. Двор небольшой и выглядит очень формально. Кроме того, "
+                       "странные огни были замечены движущимися в доме ночью.",
+        "departure": "msk",
+        "picture": "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 68000,
+        "stars": "4",
+        "country": "Доминикана",
+        "nights": 8,
+        "date": "18 января",
+    },
+    6: {
+        "title": "Light Renaissance Hotel",
+        "description": "Этот крошечный дом выглядит довольно современно и находится в ужасном состоянии. Интерьер "
+                       "выполнен в цветах, которые напоминают вам о тропическом лесу. Двор небольшой и заросший "
+                       "дикими растениями. Кроме того, это было однажды показано в телесериале, демонстрирующем "
+                       "необычно украшенные дома.",
+        "departure": "spb",
+        "picture": "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 53000,
+        "stars": "3",
+        "country": "Пакистан",
+        "nights": 13,
+        "date": "15 февраля",
+    },
+    7: {
+        "title": "King's Majesty Hotel",
+        "description": "Этот дом средних размеров выглядит немного старомодно и находится в среднем состоянии. "
+                       "Интерьер выполнен в цветах, которые напоминают о весеннем цветнике. Двор среднего размера и "
+                       "напоминает луг. Кроме того, он был построен над остатками дома, который был разрушен в "
+                       "результате пожара.",
+        "departure": "ekb",
+        "picture": "https://images.unsplash.com/photo-1468824357306-a439d58ccb1c?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 72000,
+        "stars": "5",
+        "country": "Мексика",
+        "nights": 9,
+        "date": "22 января",
+    },
+    8: {
+        "title": "Crown Hotel",
+        "description": "Этот огромный дом почти выглядит инопланетянином и находится в среднем состоянии. Интерьер "
+                       "выполнен в цветах, напоминающих апельсиновое дерево. Двор среднего размера и напоминает луг. "
+                       "Кроме того, это место печально известного убийства.",
+        "departure": "kazan",
+        "picture": "https://images.unsplash.com/photo-1549109786-eb80da56e693?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 44000,
+        "stars": "4",
+        "country": "Тайланд",
+        "nights": 7,
+        "date": "3 февраля",
+    },
+    9: {
+        "title": "Seascape Resort",
+        "description": "Этот большой дом имеет сказочный вид и находится в отличном состоянии. Интерьер выполнен в "
+                       "ярких цветах. Двор маленький и аккуратно подстрижен. На заднем дворе есть большой участок "
+                       "недавно созданной земли, а дом имеет большой решетчатый забор через него. На заднем дворе "
+                       "живут различные животные. Многие владельцы приложили согласованные усилия для поддержания "
+                       "этой собственности.",
+        "departure": "nsk",
+        "picture": "https://images.unsplash.com/photo-1570214476695-19bd467e6f7a?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 39000,
+        "stars": "3",
+        "country": "Индия",
+        "nights": 10,
+        "date": "1 февраля",
+    },
+    10: {
+        "title": "Rose Sanctum Hotel",
+        "description": "Снаружи этот дом выглядит старым, но чудесным. Он был построен из желтого соснового дерева и "
+                       "украшен белым кирпичом. Короткие, широкие окна пропускают много света и были добавлены в дом "
+                       "очень симметричным способом.",
+        "departure": "msk",
+        "picture": "https://images.unsplash.com/photo-1560200353-ce0a76b1d438?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 52000,
+        "stars": "4",
+        "country": "Куба",
+        "nights": 10,
+        "date": "30 января",
+    },
+    11: {
+        "title": "Viridian Obelisk Hotel & Spa",
+        "description": "В доме очень хороший двор с большими камнями, похожими на озеро. В задней части дома окна "
+                       "просторные, с большими окнами, они светлее, чтобы улучшить впечатление. Снаружи есть пять "
+                       "маленьких деревьев. Двор в очень хорошем состоянии и очень живописный. Есть пруд для "
+                       "развлечения",
+        "departure": "spb",
+        "picture": "https://images.unsplash.com/photo-1477120128765-a0528148fed2?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 68000,
+        "stars": "5",
+        "country": "Индия",
+        "nights": 9,
+        "date": "1 марта",
+    },
+    12: {
+        "title": "Saffron Tundra Hotel & Spa",
+        "description": "Дом оборудован огромной кухней и одной современной ванной комнатой, а также имеет огромную "
+                       "гостиную, две спальни, небольшую столовую, гостиную и скромную кладовую.  Дом чистый, "
+                       "хорошо построенный и в хорошем состоянии, но, к сожалению, кровати сгорели в мае этого года "
+                       "и, к сожалению, все еще нуждаются в ремонте. Возможно, понадобится целая команда, "
+                       "чтобы заменить старую медную топку.",
+        "departure": "kazan",
+        "picture": "https://images.unsplash.com/photo-1440151050977-247552660a3b?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 72000,
+        "stars": "4",
+        "country": "Мексика",
+        "nights": 12,
+        "date": "17 февраля",
+    },
+    13: {
+        "title": "Traveller Resort",
+        "description": "Снаружи этот дом выглядит очень элегантно. Он был построен из коричневого кирпича и имеет "
+                       "коричневые кирпичные украшения. Высокие, большие окна добавляют к общему стилю дома и были "
+                       "добавлены к дому в довольно асимметричном образце. Крыша высокая и наклонена в одну сторону и "
+                       "покрыта коричневой черепицей. Один большой дымоход высовывает центр крыши. На крыше нет окон. "
+                       "Сам дом окружен великолепным садом с виноградными лозами, пагодой, прудом и множеством разных "
+                       "цветов.",
+        "departure": "ekb",
+        "picture": "https://images.unsplash.com/photo-1553653924-39b70295f8da?ixlib=rb-1.2.1&auto=format&fit=crop&w"
+                   "=800&q=60",
+        "price": 49000,
+        "stars": "3",
+        "country": "Куба",
+        "nights": 8,
+        "date": "26 января"
+    },
+    14: {
+        "title": "History Hotel & Spa",
+        "description": "Крыша высокая, треугольная, многослойная, покрыта пшеничной соломой. Две большие трубы "
+                       "находятся по обе стороны от дома. Многие меньшие окна пропускают много света в комнаты под "
+                       "крышей.Сам дом окружен асфальтированной землей, с местом для еды и отдыха на открытом воздухе "
+                       "и различными горшечными растениями.",
+        "departure": "kazan",
+        "picture": "https://images.unsplash.com/photo-1509600110300-21b9d5fedeb7?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 91000,
+        "stars": "5",
+        "country": "Вьетнам",
+        "nights": 9,
+        "date": "3 февраля",
+    },
+    15: {
+        "title": "Riverside Lagoon Hotel & Spa",
+        "description": "Здание имеет форму круга. Дом частично окружен деревянными нависающими панелями с двух "
+                       "сторон. Второй этаж меньше первого, что позволило создать несколько балконов по бокам дома. "
+                       "Этот этаж следует тому же стилю, что и этаж ниже.",
+        "departure": "spb",
+        "picture": "https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 82000,
+        "stars": "4",
+        "country": "Доминикана",
+        "nights": 8,
+        "date": "5 февраля",
+    },
+    16: {
+        "title": "History Hotel & Spa",
+        "description": "Это высокое здание из старого кирпича. Крыша высокая, треугольная, многослойная, "
+                       "покрыта пшеничной соломой. В тесной квадратной спальне установлена деревянная и стеклянная "
+                       "мебель. Пол деревянный, а стены текстурированы и окрашены.Настенные и напольные светильники "
+                       "обеспечивают свет. Комната оформлена в цветах, напоминающих облачное небо, и в целом выглядит "
+                       "очень старомодно. Первое, что вы заметите, это тщеславие.",
+        "departure": "spb",
+        "picture": "https://images.unsplash.com/photo-1564056095795-4d63b6463dbf?ixlib=rb-1.2.1&ixid"
+                   "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": 74000,
+        "stars": "5",
+        "country": "Вьетнам",
+        "nights": 12,
+        "date": "24 января",
+    }
 
-tags = ["jazz", "blues", "rnb", "bluegrass", "lounge"]
-videos = [{
-    "id":1,
-    "title": "Как построить открытую террасу к дому своими руками",
-    "src": "http://videohost.me/hDX4hWWV5Ok.mp4",},
-    {
-    "id": 2,
-    "title": "How To Learn Anything 10x Faster",
-    "description": "Interested in learning more about Charisma University? You’re going to spend thousands and thousands hours learning over the course of your life.  What’s crazy is that despite the fact that we have all those hours sitting in the classroom, learning musical instruments, martial arts, business, language, whatever - no one teaches us how to optimize that learning experience.",
-    "src": "http://videohost.me/YLwFbwPbq.mp4",
-}]
-
-for video in videos:
-    print(video)
-
-mylist = [
-  "Capicola",
-  "Celery Root",
-  "Marjoram - Dried, Rubbed",
-  "Bread - Italian Sesame Poly",
-  "Cheese - Bocconcini",
-  "Mushroom - Shitake, Dry",
-  "French Pastry - Mini Chocolate",
-]
-
-links = [
-  {"title":"Главная","link":"/"},
-  {"title":"О Нас","link":"/about/"},
-  {"title":"Контакты","link":"/contact/"},
-]
-for i in range(len(links)):
-    print((links[i])["title"])
-
-positions=[
-  {
-    "title":"Руководитель отдела web-разработки",
-    "salary":"180 000 - 200 000",
-    "level":"lead",
-    "tags": ["ООП","Git","Flask","Redis","Управление проектами","Управление людьми",
-             "Построение команды","Ведение переговоров"],
-  },
-  {
-    "title":"Python Developer",
-    "salary":"200 000",
-    "level":"middle",
-    "tags":["Python","Django","PostgreSQL","Linux","Git"],
-  },
-  {
-    "title":"Python developer",
-    "salary":"130 000 - 180 000",
-    "level":"middle",
-    "tags":["Django","Python","Flask","PostgreSQL","Высоконагруженные системы","Git","Docker"],
-   }
-]
-
-carriers=[
-  {"name": "American Airlines", "planes": 950, "founded": 1934, "passengers": 193.7},
-  {"name": "Lufthansa", "planes": 351, "founded": 1955 , "passengers": 77.5},
-  {"name": "Ryanair", "planes": 438, "founded": 1984,  "passengers": 66.5},
-  {"name": "Southwest Airlines", "planes": 746,"founded": 1971,"passengers": 101.3},
-  {"name": "Delta", "planes": "809", "founded": "1929" , "passengers": 161.1},
-]
-
-
-
-movies = [
-  {
-    "title":"El Camino: A Breaking Bad Movie",
-    "rutitle":"Путь: Во все тяжкие. Фильм",
-    "genres":["Криминальный","Драма"],
-    "imdbrate":8.8,
-  },
-  {
-    "title": "The Irishman",
-    "rutitle": "Ирландец",
-    "genres": ["Криминальный", "Исторический"],
-    "imdbrate": 8.7,
-  },
-  {
-    "title": "Downton Abbey",
-    "rutitle": "Аббатство Даунтон",
-    "genres": ["Драма"],
-    "imdbrate": 8.7,
-  },
-  {
-    "title": "Avengers Endgame",
-    "rutitle": "Мстители: Финал",
-    "genres": ["Приключения", "Фантастика", "Боевик"],
-    "imdbrate": 8.6,
-  },
-  {
-    "title": "Joker",
-    "rutitle": "Джокер",
-    "genres": ["Криминальный", "Триллер", "Драма"],
-    "imdbrate": 8.7,
-  },
-]
-
-
-
-
-
-
+}
 
 @app.route('/')
 def open_index_page():
-    return render_template('index.html')
-
-@app.route('/tour')
-def open_tour_page():
-    return render_template('tour.html')
-
-@app.route('/departure')
-def open_departure_page():
-    return render_template('departure.html')
-
-@app.route('/data')
-def data_index_page():
-    return render_template('data.html',
-                           tags=tags,
-                           videos=videos,
-                           points=points,
-                           mylist=mylist,
-                           links=links,
-                           positions=positions,
-                           carriers=carriers,
-                           movies=movies)
+    return render_template('index.html',
+                           tours=tours,
+                           title=title,
+                           subtitle=subtitle,
+                           description=description)
 
 
-@app.route('/base_2')
-def base_page():
-    return render_template('base_2.html')
+@app.route('/tour/<number>')
+def open_tour_page(number):
+    return render_template('tour.html',
+                           tours=tours[int(number)],
+                           departures=departures
+                           )
+
+@app.route('/departure/<town>')
+def open_departure_page(town):
+    tours_2 = {k: d for k, d in tours.items() if d["departure"] == town}
+    return render_template('departure.html',
+                           town=town,
+                        tours=tours_2,
+                        departures=departures
+    )
 
 
+@app.errorhandler(500)
+def render_server_error(error):
+    return render_template('error.html')
+
+@app.errorhandler(404)
+def render_server_error(error):
+    return render_template('error.html')
 
 if __name__ == '__main__':
     app.run()
-
-#{ % for tags_inside in {{k.tags}} %}
-#< ul > {{tags_inside}} < / ul >
-#{ % endfor %}
